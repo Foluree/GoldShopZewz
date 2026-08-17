0b8760cbff961cdca7c49e2a70ad9131e403de76
Foluree <9284111970olga@gmail.com>
2026-08-17 16:10:43 +0300

    added check token with payment.html

--- app/profil/test.py
+++ app/profil/test.py
@@ -15,10 +15,10 @@ async def payment_page(request: Request, session: AsyncSession = Depends(get_ses
     token = request.cookies.get("booking_accses_token")
     user_id = verify_accses_token(token)
     if not user_id:
-        return RedirectResponse(url="/regist", status_code=303)
+        return RedirectResponse(url="/regist/", status_code=303)
 
     auth_user = await load_auth_user(session, int(user_id))
     if not auth_user:
         return RedirectResponse(url="/regist/", status_code=303)
-    
+
     return FileResponse("app/templates/payment_1.html")
